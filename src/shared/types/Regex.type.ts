de488ea7e717f57e05600456c8d243fbd43adcac
Wesley Sousa <wesleytuck@outlook.com>
2023-11-06 09:54:47 -0300

    melhoria validações

--- src/shared/types/Regex.type.ts
+++ src/shared/types/Regex.type.ts
@@ -1,3 +1,5 @@
 export const RegexEmail = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/
 export const RegexStringNumber = /^[0-9]+$/
+export const RegexPhone = /^\d{11}$/
+export const RegexCep = /^\d{8}$/
 export const RegexCpfCnpj = /^\d{11}$|^\d{14}$/
