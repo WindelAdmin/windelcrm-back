e0f0cab7e6f35958817b17d4b121fb728e21e93d
Wesley Sousa <wesleytuck@outlook.com>
2023-11-06 08:52:08 -0300

    ajuste

--- src/Route.module.ts
+++ src/Route.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common'
 import { RouterModule } from '@nestjs/core'
+import CompanyModule from './modules/company/Company.module'
 import { UserModule } from './modules/user/User.module'
 
 @Module({
   imports: [
     RouterModule.register([
       {
-        path: 'person',
-        children: [
-          {
-            path: 'user',
-            module: UserModule
-          }
-        ]
+        path: 'user',
+        module: UserModule
+      },
+      {
+        path: 'company',
+        module: CompanyModule
       }
     ])
   ]
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,17 @@
 import { Module } from '@nestjs/common'
-import { APP_GUARD, RouterModule } from '@nestjs/core'
+import { APP_GUARD } from '@nestjs/core'
+import { RouteModule } from './Route.module'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { JwtAuthGuard } from './infra/http/guards/jwt-auth.guard'
 import { AuthModule } from './modules/auth/auth.module'
 import CompanyModule from './modules/company/Company.module'
-import { UserModule } from './modules/user/User.module'
 
 @Module({
   imports: [
     AuthModule,
     CompanyModule,
-    RouterModule.register([
-      {
-        path: 'user',
-        module: UserModule
-      },
-      {
-        path: 'company',
-        module: CompanyModule
-      }
-    ])
-  ],
+    RouteModule],
   controllers: [AppController],
   providers: [
     AppService,
