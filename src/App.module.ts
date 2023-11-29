e0d0bdbddf5af29988883c767e4dca9430d1c49f
Wesley Sousa <wesleytuck@outlook.com>
2023-11-29 17:18:26 -0300

    Ajustes de rotas e módulos

--- src/App.module.ts
+++ src/App.module.ts
@@ -1,16 +1,39 @@
 import { UserContextModule } from '@modules/aux/contexts/UserContext.module'
 import { Module } from '@nestjs/common'
-import { APP_GUARD } from '@nestjs/core'
+import { APP_GUARD, RouterModule } from '@nestjs/core'
 import { AuthModule } from '@src/modules/aux/auth/auth.module'
 import CompanyModule from '@src/modules/core/company/Company.module'
 import { UserModule } from '@src/modules/core/user/User.module'
 import { CryptoModule } from '@src/modules/generic/crypto/Crypto.module'
-import { RouteModule } from './Route.module'
 import { JwtAuthGuard } from './infra/http/guards/jwt-auth.guard'
 import PrismaModule from './infra/persistence/Prisma.module'
+import PermissionModule from './modules/core/user/permission/Permission.module'
 
 @Module({
-  imports: [CryptoModule, AuthModule, UserContextModule, UserModule, CompanyModule, RouteModule, PrismaModule],
+  imports: [CryptoModule, AuthModule, UserContextModule, UserModule, CompanyModule, PrismaModule, RouterModule.register([
+      {
+        path: 'user',
+        module: UserModule,
+        children: [
+          {
+            path: 'permission',
+            module: PermissionModule
+          }
+        ]
+      },
+      {
+        path: 'company',
+        module: CompanyModule
+      },
+      {
+        path: 'person',
+        children: [
+          {
+            path: 'employee'
+          }
+        ]
+      }
+    ])],
   providers: [
     {
       provide: APP_GUARD,
--- src/Route.module.ts
+++ src/Route.module.ts
@@ -2,13 +2,20 @@ import { Module } from '@nestjs/common'
 import { RouterModule } from '@nestjs/core'
 import CompanyModule from '@src/modules/core/company/Company.module'
 import { UserModule } from '@src/modules/core/user/User.module'
+import PermissionModule from './modules/core/user/permission/Permission.module'
 
 @Module({
   imports: [
     RouterModule.register([
       {
         path: 'user',
-        module: UserModule
+        module: UserModule,
+        children: [
+          {
+            path: 'permission',
+            module: PermissionModule
+          }
+        ]
       },
       {
         path: 'company',
